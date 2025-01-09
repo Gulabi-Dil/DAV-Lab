e55d675a0ef49197bf945b79ec062a4f55c97bd0
Gulabi-Dil <thegreatmindhehe@gmail.com>
2025-01-09 20:09:44 +0530

    Update and rename Calculator.py to 1) Calculator.py

--- Week 1/1) Calculator.py
+++ Week 1/1) Calculator.py
@@ -1,3 +1,4 @@
+## Write a Python function to input two numbers and perform the Calculator operations of (+, -, *, /).
 a=int(input("Enter first number "))
 b=int(input("Enter second number "))
 print(f"{a} + {b} = {a+b}\n{a} - {b} = {a-b}\n{a} * {b} = {a*b}\n{a} / {b} = {a/b}")
