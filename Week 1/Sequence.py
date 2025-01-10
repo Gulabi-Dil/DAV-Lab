f39191aabdf586a0e1b97ecf7214b7ba435791f2
Gulabi-Dil <thegreatmindhehe@gmail.com>
2025-01-10 10:32:06 +0530

    Create Sequence.py

---
+++ Week 1/Sequence.py
@@ -0,0 +1,5 @@
+## Create a Python function that creates a sequence between 1 and 100 and prints all the odd numbers. Compute and display the sum of all the even numbers.
+from sympy import *
+def Seq():
+  return [x for x in range(1,101) if x%2==1]
+for n in Seq(): print(n, end=" ")
